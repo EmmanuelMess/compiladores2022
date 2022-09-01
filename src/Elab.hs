a6ab70da8f71bf7f82338c94f382eca9a07971c3
EmmanuelMess <emmanuelbendavid@gmail.com>
2022-09-01 06:35:14 -0300

    Menos casos elab'

--- src/Elab.hs
+++ src/Elab.hs
@@ -30,12 +30,11 @@ elab' env (SV p v) =
 
 elab' _ (SConst p c) = Const p c
 elab' env (SLam p (v,ty) t) = Lam p v ty (close v (elab' (v:env) t))
-elab' env (SSugar (SugarLam p ((v,ty):(xs@(_:_))) t)) =
+elab' env (SSugar (SugarLam p xs t)) =
   let
-    t' = SSugar (SugarLam p xs t)
-  in elab' env (SLam p (v,ty) t')
-elab' env (SSugar (SugarLam p [(v,ty)] t)) =
-  elab' env (SLam p (v,ty) t)
+    a = map (\(v,ty) -> SLam p (v,ty)) xs
+    t' = foldr (\t1 t2 -> t1 t2) t a
+  in elab' env t'
 elab' env (SFix p (f,fty) (x,xty) t) = Fix p f fty x xty (close2 f x (elab' (x:f:env) t))
 elab' env (SIfZ p c t e)         = IfZ p (elab' env c) (elab' env t) (elab' env e)
 -- Operadores binarios
@@ -52,16 +51,11 @@ elab' env (SSugar (SugarLetFun p (functionName,params,returnType) sugarDef body)
     vty = foldr FunTy returnType (map snd params)
     v = functionName
   in elab' env (SLet p (v, vty) def body)
-elab' env (SSugar (SugarLetFunRec p (functionName,param:(lamParams@(_:_)),returnType) sugarDef body)) =
+elab' env (SSugar (SugarLetFunRec p (functionName,(v,ty):lamParams,returnType) sugarDef body)) =
   let
-    def = SSugar (SugarLam p lamParams sugarDef)
     vty = foldr FunTy returnType (map snd lamParams)
-  in elab' env (SSugar (SugarLetFunRec p (functionName,[param],vty) def body))
-elab' env (SSugar (SugarLetFunRec p (functionName,[(v,ty)],returnType) sugarDef body)) =
-  let
-    vty = FunTy ty returnType
-    def = SFix p (functionName,vty) (v,ty) sugarDef
-    v' = functionName
-  in elab' env (SLet p (v', vty) def body)
+    fixty = FunTy ty vty
+    def = SFix p (functionName,fixty) (v,ty) (SSugar (SugarLam p lamParams sugarDef))
+  in elab' env (SLet p (functionName,fixty) def body)
 elabDecl :: Decl STerm -> Decl Term
 elabDecl = fmap elab
