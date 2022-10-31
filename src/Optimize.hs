93dd881e5fb27b5ecd617348802b25a5f86b19ce
EmmanuelMess <emmanuelbendavid@gmail.com>
2022-10-30 22:53:15 -0300

    Inline y expand

--- src/Optimize.hs
+++ src/Optimize.hs
@@ -13,6 +13,7 @@ module Optimize where
 import Data.Maybe
 
 import Eval ( semOp )
+import Subst ( subst, varChanger )
 import Lang
 import MonadFD4
 
@@ -22,10 +23,77 @@ letWithPrint = ""
 optimize :: MonadFD4 m => Decl TTerm -> m (Decl TTerm)
 optimize (Decl p n ty t) =
   do
-    t' <- deadCodeElimination t
-    t'' <- constantFoldingAndPropagation t'
-    return (Decl p n ty t'')
+    t1 <- deadCodeElimination t
+    t2 <- constantFoldingAndPropagation t1
+    t3 <- inlineExpansion t2
+    t4 <- constantFoldingAndPropagation t3
+    return (Decl p n ty t4)
 optimize d = return d
+
+
+inlineExpansion :: MonadFD4 m => TTerm -> m TTerm
+inlineExpansion t =
+  do
+    let t' = inline t
+    let t'' = expand t'
+    return t''
+
+inline :: TTerm -> TTerm
+inline t@(Let p v ty def@(Lam _ _ _ _) (Sc1 t1)) =
+  let
+    t1' = inline t1
+    def' = inline def
+  in if doInlineExpansion t1'
+     then subst def' (Sc1 t1')
+     else Let p v ty def' (Sc1 t1')
+inline (Let p v ty def (Sc1 t)) = Let p v ty (inline def) (Sc1 (inline t))
+inline (Fix p x xty y yty (Sc2 t)) = Fix p x xty y yty (Sc2 (inline t)) -- TODO Fix
+inline t@(V _ _) = t
+inline t@(Const _ _) = t
+inline (Lam p n ty (Sc1 t)) = Lam p n ty (Sc1 (inline t))
+inline (App p l r) = App p (inline l) (inline r)
+inline (Print p str t) = Print p str (inline t)
+inline (BinaryOp p op t u) = BinaryOp p op (inline t) (inline u)
+inline (IfZ p c t e) = IfZ p (inline c) (inline t) (inline e)
+
+expand :: TTerm -> TTerm
+expand (App _ l@(Lam _ _ _ (Sc1 t)) r) = subst (expand r) (Sc1 (expand t))
+expand (App p l r) = App p (expand l) (expand r)
+expand t@(V _ _) = t
+expand t@(Const _ _) = t
+expand (Lam p n ty (Sc1 t)) = Lam p n ty (Sc1 (expand t))
+expand (Print p str t) = Print p str (expand t)
+expand (BinaryOp p op t u) = BinaryOp p op (expand t) (expand u)
+expand (Fix p x xty y yty (Sc2 t)) = Fix p x xty y yty (Sc2 (expand t))
+expand (IfZ p c t e) = IfZ p (expand c) (expand t) (expand e)
+expand (Let p n nty y (Sc1 t)) = Let p n nty (expand y) (Sc1 (expand t))
+
+doInlineExpansion :: TTerm -> Bool
+doInlineExpansion t = (numberOfCalls 0 t) * (costScore t) <= 60
+
+numberOfCalls :: Int -> TTerm -> Int
+numberOfCalls n (V _ (Bound i)) = if n == i then 1 else 0
+numberOfCalls n (V _ _) = 0
+numberOfCalls n (Lam _ _ _ (Sc1 t)) = numberOfCalls (n+1) t
+numberOfCalls n (App _ l r) = (numberOfCalls n l) + (numberOfCalls n r)
+numberOfCalls n (Print _ _ t) = (numberOfCalls n t)
+numberOfCalls n (BinaryOp _ _ t u) = (numberOfCalls n t) + (numberOfCalls n u)
+numberOfCalls n (Fix _ _ _ _ _ (Sc2 t)) = (numberOfCalls (n+2) t)
+numberOfCalls n (IfZ _ c t e) = (numberOfCalls n c) + (numberOfCalls n t) + (numberOfCalls n e)
+numberOfCalls n (Const _ _) = 0
+numberOfCalls n (Let _ _ _ e (Sc1 t)) = (numberOfCalls n e) + (numberOfCalls (n+1) t)
+
+costScore :: TTerm -> Int
+costScore (V _ (Bound i)) = 2
+costScore (V _ _) = 0
+costScore (Lam _ _ _ (Sc1 t)) = 2 + (costScore t) + 1
+costScore (App _ l r) = (costScore l) + (costScore r) + 1
+costScore (Print _ str t) = (length str) + 1 + 1 + (costScore t) + 1
+costScore (BinaryOp _ _ t u) = (costScore t) + (costScore u) + 1
+costScore (Fix _ _ _ _ _ (Sc2 t)) = 2 + (costScore t) + 2
+costScore (IfZ _ c t e) = (costScore c) + 1 + (costScore t) + 2 + (costScore e)
+costScore (Const _ _) = 2
+costScore (Let _ _ _ e (Sc1 t)) = (costScore e) + 1 + (costScore t) + 1
 
 
 deadCodeElimination :: MonadFD4 m => TTerm -> m TTerm
@@ -84,8 +152,7 @@ findPrint (Let _ _ _ t1 (Sc1 t2)) = findPrint t1 || findPrint t2
 
 constantFoldingAndPropagation :: MonadFD4 m => TTerm -> m TTerm
 constantFoldingAndPropagation t@(V _ (Bound _)) =
-  do
-    return t
+  return t
 constantFoldingAndPropagation t@(V _ (Free _)) = undefined
 constantFoldingAndPropagation (V _ (Global n)) =
   do
@@ -116,7 +183,7 @@ constantFoldingAndPropagation (App p l r) =
   do
     l' <- constantFoldingAndPropagation l
     r' <- constantFoldingAndPropagation r
-    return (App p l' r') --TODO fix constant functions
+    return (App p l' r')
 constantFoldingAndPropagation (Fix p f fty x xty (Sc2 t)) =
   do
     t' <- constantFoldingAndPropagation t
@@ -125,7 +192,7 @@ constantFoldingAndPropagation (Let p v ty def (Sc1 t)) =
   do
     def' <- constantFoldingAndPropagation def
     t' <- constantFoldingAndPropagation t
-    return (Let p v ty def' (Sc1 t'))
+    return (optLet (Let p v ty def' (Sc1 t')))
 constantFoldingAndPropagation (BinaryOp p op l r) =
   do
     l' <- constantFoldingAndPropagation l
@@ -147,3 +214,8 @@ optSum (BinaryOp p Add l r@(Const _ _)) = BinaryOp p Add r l
 optSum (BinaryOp p op l (Const _ (CNat 0))) = l
 optSum (BinaryOp p Add (Const _ (CNat 0)) r) = r
 optSum t = t
+
+optLet :: TTerm -> TTerm
+optLet (Let _ _ _ def@(Const _ _) s) = subst def s
+optLet (Let _ _ _ def@(V _ _) s) = subst def s
+optLet t = t
