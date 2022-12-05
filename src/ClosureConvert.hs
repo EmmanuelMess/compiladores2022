04f2b115ecdf9006a44e6f26caa97fa1766f1dbf
EmmanuelMess <emmanuelbendavid@gmail.com>
2022-12-04 22:27:27 -0300

    Arreglados problemas de compilacion, rotos los tipos en ClosureConvert

--- src/ClosureConvert.hs
+++ src/ClosureConvert.hs
@@ -15,74 +15,84 @@ import Control.Monad.Writer
 
 import IR
 import Lang
+import Subst
 
-freshName :: () -> StateT Int (Writer [IrDecl]) Name
-freshName () = do
+import C ( ir2C )
+
+freshName :: StateT Int (Writer [IrDecl]) Name
+freshName = do
     counter <- get
     (modify (+1))
-    let fresh_name = "__" ++ "f" ++ show (counter)
-    return fresh_name
+    let freshName = "__" ++ "f" ++ show (counter)
+    return freshName
 
-closeIr :: [Name] -> Name -> Ir -> Ir --fijar acceso a var libres
-closeIr freevars closname term = foldr (\(arg, i) ir -> IrLet arg (IrAccess (IrVar closname) i) ir) term (zip freevars [1..])
+closeIr :: [Name] -> Name -> Ir -> Ir
+closeIr freevars closname term =
+  let f (name, i) ir = IrLet name IrInt (IrAccess (IrVar closname) IrInt i) ir -- TODO types are wrong in this line
+  in foldr f term (zip freevars [1..])
 
---Falta Hacer el LAM y FIX
 closureConvert :: Term -> StateT Int (Writer [IrDecl]) Ir
-closureConvert (V _ (Bound i)) = error "Bound en cc" -- Listo
-closureConvert (V _ (Free _)) = return $ IrVar name --Listo
-closureConvert (V _ (Global _)) = return $ IrGlobal name --Listo
-closureConvert (Const _ c) = return $ IrConst c  --Listo
-closureConvert (Lam _ n ty (Sc1 t)) = --Listo
+closureConvert (V _ (Bound i)) = error "Bound en cc"
+closureConvert (V _ (Free n)) = return $ IrVar n
+closureConvert (V _ (Global n)) = return $ IrGlobal n
+closureConvert (Const _ c) = return $ IrConst c
+closureConvert (Lam _ n ty s@(Sc1 t)) =
   do
-     newName <- freshName () ++ "__"++n
+     newName <- freshName
+     let completeName = newName ++ "__" ++ n
      let freevars = freeVars t
-            closureName = newName ++ "__closure"
-            varName = newName ++ "__argument"
-     term' <- closeIr freevars closureName <$> closureConvert (open varName t)
-     tell [IrFun freshname [closureName, varName] term']
-     return $ MkClosure freshname (fmap IrVar freevars)
-closureConvert (App _ t1 t2) = --Listo
+     let closureName = completeName ++ "__closure"
+     let varName = completeName ++ "__argument"
+     t' <- closeIr freevars closureName <$> closureConvert (open varName s)
+
+     newName1 <- freshName
+     tell [IrFun newName1 IrInt [(closureName, IrInt), (varName, IrInt)] t'] -- TODO types are wrong in this line
+
+     newName2 <- freshName
+     return $ MkClosure newName2 (fmap IrVar freevars)
+closureConvert (App _ t1 t2) =
   do
-    term1' <- closureConvert term1
-    term2' <- closureConvert term2
-    name <- freshName ()
-    return $ IrLet name term1' (IrCall (IrAccess (IrVar name) 0) [IrVar name, term2'])
-closureConvert (Print _ str t) = --Listo
+    t1' <- closureConvert t1
+    t2' <- closureConvert t2
+    name <- freshName
+    return $ IrLet name IrInt t1' $ IrCall (IrAccess (IrVar name) IrInt 0) [IrVar name, t2'] IrInt -- TODO types are wrong in this line
+closureConvert (Print _ str t) =
   do
     t' <- closureConvert t
     return (IrPrint str t')
-closureConvert (BinaryOp _ op t1 t2) = --Listo
+closureConvert (BinaryOp _ op t1 t2) =
   do
     t1' <- closureConvert t1
     t2' <- closureConvert t2
     return (IrBinaryOp op t1' t2')
 closureConvert (Fix _ x xty n nty (Sc2 t)) = undefined -- TODO
-closureConvert (IfZ _ c t1 t2) = --Listo
+closureConvert (IfZ _ c t1 t2) =
   do
     c' <- closureConvert c
     t1' <- closureConvert t1
     t2' <- closureConvert t2
     return (IrIfZ c' t1' t2')
-closureConvert (Let _ n ty t1 (Sc1 t2)) = --Listo
+closureConvert (Let _ n ty t1 (Sc1 t2)) =
   do
     let ty' = typeConvert ty
     t1' <- closureConvert t1
     t2' <- closureConvert t2
     return (IrLet n ty' t1' t2')
 
+typeConvert :: Ty -> IrTy
+typeConvert (NamedTy _) = undefined
+typeConvert NatTy = IrInt
+typeConvert (FunTy _ _) = IrFunTy
 
---typeConvert :: Ty -> IrTy
---typeConvert (NamedTy _) = undefined
---typeConvert NatTy = IrInt
---typeConvert (FunTy _ _) = IrFunTy
-
-runCC :: [Decl Term Ty] -> [IrDecl]
+runCC :: [Decl Term] -> [IrDecl]
 runCC decls = snd $ runWriter $ runStateT (go decls) 0
               where go [] = return []
-                    go ((Decl _ name _ body):decls') = do body' <- closureConvert body
-                                                          let irdecl = IrVal name body'
-                                                          tell [irdecl]
-                                                          go decls'
+                    go ((Decl _ name _ body):decls') =
+                      do
+                        body' <- closureConvert body
+                        let irdecl = IrVal name IrInt body' -- TODO types are wrong in this line
+                        tell [irdecl]
+                        go decls'
 
 compileC :: [Decl Term] -> String
 compileC xs = ir2C $ IrDecls $ runCC xs
