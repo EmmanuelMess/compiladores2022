db9f4677a2c55edaa823c08726610ad3bf347587
EmmanuelMess <emmanuelbendavid@gmail.com>
2023-01-30 23:11:02 -0300

    Mejoras en los nombres

--- src/ClosureConvert.hs
+++ src/ClosureConvert.hs
@@ -13,42 +13,48 @@ module ClosureConvert where
 import Control.Monad.State
 import Control.Monad.Writer
 
+import Data.Maybe
+
 import IR
 import Lang
 import Subst
 
 import C ( ir2C )
 
-freshName :: (String -> String) -> StateT Int (Writer [IrDecl]) Name
-freshName f = do -- TODO make fresh variables relate to actual fd4 code, it is unreadable
-    counter <- get
-    modify (+1)
-    return $ f $ show counter
+freshName :: String -> StateT ([(String, Int)]) (Writer [IrDecl]) Name
+freshName str = do -- TODO make fresh variables relate to actual fd4 code, it is unreadable
+    counters <- get
+    let update c = case (lookup str c) of
+                      Nothing -> (str, 0):c
+                      Just x -> (str, x+1):(filter ((/=str) . fst) c)
+    modify update
+    let counter = fromMaybe 0 (lookup str counters)
+    return $ str++(show counter)
 
 closeIr :: [Name] -> Name -> Ir -> Ir
 closeIr freevars closname term =
   let f (name, i) ir = IrLet name IrInt (IrAccess (IrVar closname) IrInt i) ir -- TODO types are wrong in this line
   in foldr f term (zip freevars [1..])
 
-closureConvert :: TTerm -> StateT Int (Writer [IrDecl]) Ir
+closureConvert :: TTerm -> StateT ([(String, Int)]) (Writer [IrDecl]) Ir
 closureConvert (V (_, ty) (Bound i)) =
  do
-   name <- freshName (\c -> "bound"++c)
+   name <- freshName "bound"
    return $ IrAccess (IrVar name) (typeConvert ty) i
 closureConvert (V _ (Free n)) = return $ IrVar n
 closureConvert (V _ (Global n)) = return $ IrGlobal n
 closureConvert (Const _ c) = return $ IrConst c
-closureConvert (Lam _ n ty s@(Sc1 t)) =
+closureConvert (Lam _ n ty s@(Sc1 t)) = -- TODO fix?
   do
      let freevars = freeVars t
-     varName <- freshName (\c -> "lam"++c)
+     varName <- freshName "lam"
      let closureName = "clos"++varName
      t' <- closeIr freevars closureName <$> closureConvert (open varName s)
 
-     newName1 <- freshName (\c -> "fun"++c)
+     newName1 <- freshName "fun"
      tell [IrFun newName1 IrInt [(closureName, IrInt), (varName, IrInt)] t'] -- TODO types are wrong in this line
 
-     newName2 <- freshName (\c -> "clos"++c)
+     newName2 <- freshName "clos"
      return $ MkClosure newName2 (fmap IrVar freevars)
 closureConvert (App (_, ty) t1 t2) =
   do
@@ -57,7 +63,7 @@ closureConvert (App (_, ty) t1 t2) =
     t1' <- closureConvert t1
     t2' <- closureConvert t2
 
-    name <- freshName (\c -> "app"++c)
+    name <- freshName "app"
     let appResult = IrCall (IrAccess (IrVar name) t1ty 0) [IrVar name, t2'] (typeConvert ty)
     return $ IrLet name t1ty t1' $ appResult
 closureConvert (Print _ str t) =
@@ -89,7 +95,7 @@ typeConvert NatTy = IrInt
 typeConvert (FunTy _ _) = IrFunTy
 
 runCC :: [Decl TTerm] -> [IrDecl]
-runCC decls = snd $ runWriter $ runStateT (go decls) 0
+runCC decls = snd $ runWriter $ runStateT (go decls) []
               where go [] = return []
                     go ((Decl _ name _ body):decls') =
                       do
